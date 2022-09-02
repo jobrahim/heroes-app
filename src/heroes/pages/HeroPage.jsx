ecd3ce83990d8bc2dbcfab1c089ae0bea0bd8e9d
Ibra <jicastellanos@stefanini.com>
2022-09-02 14:05:39 -0300

    first commit

---
+++ src/heroes/pages/HeroPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroById } from '../helpers';
+
+
+export const HeroPage = () => {
+
+    const {id} = useParams();
+
+   const hero = getHeroById(id);
+   
+   if(!hero){
+    return (<Navigate to='/' />);
+   }
+
+  return (
+
+        <h1>{hero.superhero}</h1>
+    
+    
+  )
+}
